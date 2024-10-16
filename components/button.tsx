d3a8863cf7b75bf87a4a30bd9c353b2820a48e52
Isaque <eu@rtrampox.com.br>
2024-10-16 04:57:34 -0300

    Code fixes to comply with linting rules.

--- components/button.tsx
+++ components/button.tsx
@@ -1,71 +1,82 @@
-import { createContext, useContext } from "react"
+import { createContext, useContext } from "react";
 import {
-    Text,
-    TextProps,
-    TouchableOpacity,
-    ActivityIndicator,
-    TouchableOpacityProps,
-} from "react-native"
-import clsx from "clsx"
+	Text,
+	type TextProps,
+	TouchableOpacity,
+	ActivityIndicator,
+	type TouchableOpacityProps,
+} from "react-native";
+import clsx from "clsx";
 
-type Variants = "primary" | "secondary" | "destructive" | "destructive-outlined" | "outline" | "outline-dark"
+type Variants =
+	| "primary"
+	| "secondary"
+	| "destructive"
+	| "destructive-outlined"
+	| "outline"
+	| "outline-dark";
 
 type ButtonProps = TouchableOpacityProps & {
-    variant?: Variants
-    isLoading?: boolean
-}
+	variant?: Variants;
+	isLoading?: boolean;
+};
 
-const ThemeContext = createContext<{ variant?: Variants }>({})
+const ThemeContext = createContext<{ variant?: Variants }>({});
 
 function Button({
-    variant = "primary",
-    children,
-    isLoading,
-    className,
-    ...rest
+	variant = "primary",
+	children,
+	isLoading,
+	className,
+	...rest
 }: ButtonProps) {
-    return (
-        <TouchableOpacity
-            className={clsx(
-                "h-11 flex-row items-center justify-center rounded-lg gap-2 px-2",
-                {
-                    "bg-lime-300": variant === "primary",
-                    "bg-zinc-800": variant === "secondary",
-                    "bg-red-700/75": variant === "destructive",
-                    "bg-red-700/10 border border-red-700": variant === "destructive-outlined",
-                    "bg-zinc-800/10 border border-zinc-800": variant === "outline",
-                    "bg-neutral-900/20 border border-neutral-900": variant === "outline-dark",
-                },
-                className
-            )}
-            activeOpacity={0.7}
-            disabled={isLoading}
-            {...rest}
-        >
-            <ThemeContext.Provider value={{ variant }}>
-                {isLoading ? <ActivityIndicator className="text-zinc-200" /> : children}
-            </ThemeContext.Provider>
-        </TouchableOpacity>
-    )
+	return (
+		<TouchableOpacity
+			className={clsx(
+				"h-11 flex-row items-center justify-center rounded-lg gap-2 px-2",
+				{
+					"bg-lime-300": variant === "primary",
+					"bg-zinc-800": variant === "secondary",
+					"bg-red-700/75": variant === "destructive",
+					"bg-red-700/10 border border-red-700":
+						variant === "destructive-outlined",
+					"bg-zinc-800/10 border border-zinc-800": variant === "outline",
+					"bg-neutral-900/20 border border-neutral-900":
+						variant === "outline-dark",
+				},
+				className,
+			)}
+			activeOpacity={0.7}
+			disabled={isLoading}
+			{...rest}
+		>
+			<ThemeContext.Provider value={{ variant }}>
+				{isLoading ? <ActivityIndicator className="text-zinc-200" /> : children}
+			</ThemeContext.Provider>
+		</TouchableOpacity>
+	);
 }
 
 function Title({ children }: TextProps) {
-    const { variant } = useContext(ThemeContext)
+	const { variant } = useContext(ThemeContext);
 
-    return (
-        <Text
-            className={clsx("text-base font-semibold", {
-                "text-lime-950": variant === "primary",
-                "text-zinc-200": variant === "secondary",
-                "text-zinc-200 text-center": variant === "destructive" || variant === "destructive-outlined" || variant === "outline-dark",
-                "text-zinc-300": variant === "outline",
-            })}
-        >
-            {children}
-        </Text>
-    )
+	return (
+		<Text
+			className={clsx("text-base font-semibold", {
+				"text-lime-950": variant === "primary",
+				"text-zinc-200": variant === "secondary",
+				"text-zinc-200 text-center":
+					variant === "destructive" ||
+					variant === "destructive-outlined" ||
+					variant === "outline-dark",
+				"text-zinc-300": variant === "outline",
+			})}
+		>
+			{children}
+		</Text>
+	);
 }
 
-Button.Title = Title
+Button.Title = Title;
 
-export { Button }
+export { Button };
